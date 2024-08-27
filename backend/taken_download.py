e66091e7aa177e1650955ab7a5d254b9e127e67a
MorrisNijland3 <morrisnijland@gmail.com>
2024-08-27 16:27:38 +0200

    update

---
+++ backend/taken_download.py
@@ -0,0 +1,23 @@
+import requests
+import os
+import sys
+import json
+
+url = 'https://youngsparks.sharepoint.com/:u:/s/YoungSparksintern/EfaANmPQWrJPhArpcb19-v4BCK8VIpXoxhWWrfsIw23Vdg?e=z0HuJn&download=1'
+filename = 'taken.json'
+
+def get_script_directory():
+    if getattr(sys, 'frozen', False):
+        return os.path.dirname(sys.executable)
+    else:
+        return os.path.dirname(os.path.abspath(__file__))
+
+script_dir = get_script_directory()
+json_path = os.path.join(script_dir, filename)
+
+response = requests.get(url)
+if response.status_code == 200:
+    with open(json_path, 'w') as file:
+        json.dump(response.json(), file, indent=4)
+else:
+    print("Failed to download the file.")
